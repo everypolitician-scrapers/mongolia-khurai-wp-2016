aa4f3ead16acb373345c27036d07b38aaf2faaac
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-19 10:42:23 +0100

    Add TermPage class
    Moves #members and #table from Class to subclass TermPage.

--- lib/page.rb
+++ lib/page.rb
@@ -8,19 +8,11 @@ class Page
     @url = url
   end
 
-  def members
-    Table.new(table).rows
-  end
-
   private
 
   attr_reader :url
 
   def noko
     @noko ||= Nokogiri::HTML(open(url).read)
-  end
-
-  def table
-    noko.xpath('.//h2/span[text()[contains(.,"Constituency")]]/following::table[1]')
   end
 end
