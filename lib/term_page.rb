e16cc39abb1516b5dab8c013e0bf2916796214a8
Tony Bowden <tony@tmtm.com>
2016-10-22 08:34:05 +0100

    Consolidate Page and TermPage
    We're only dealing with a single type of page, so there's no need for a
    base class here yet.

--- lib/term_page.rb
+++ lib/term_page.rb
@@ -1,12 +1,25 @@
-require_relative 'page'
 require_relative 'members'
+require 'field_serializer'
+require 'nokogiri'
 
-class TermPage < Page
+class TermPage
+  include FieldSerializer
+
+  def initialize(url)
+    @url = url
+  end
+
   field :members do
     Members.new(table).to_a
   end
 
   private
+
+  attr_reader :url
+
+  def noko
+    @noko ||= Nokogiri::HTML(open(url).read)
+  end
 
   def table
     noko.xpath('.//h2/span[text()[contains(.,"Constituency")]]/following::table[1]')
