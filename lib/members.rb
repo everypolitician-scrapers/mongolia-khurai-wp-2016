36ec11a193a4d1c763276c54f39666843da0b263
Tony Bowden <tony@tmtm.com>
2016-10-22 08:39:36 +0100

    Make 'Members' a NokogiriDocument
    This is exactlly what NokogiriDocument was created to represent.

--- lib/members.rb
+++ lib/members.rb
@@ -1,20 +1,13 @@
+require_relative 'nokogiri_document'
 require_relative 'khural_member'
 
-class Members
-  def initialize(node)
-    @table = node
-  end
-
+class Members < NokogiriDocument
   def to_a
     constituency = nil
-    table.xpath('.//tr[td]').map do |tr|
+    noko.xpath('.//tr[td]').map do |tr|
       tds = tr.xpath('./td')
       constituency = tds.shift.text.strip.gsub("\n",' — ') if tds.first[:rowspan]
       KhuralMember.new(tds).to_h.merge(constituency: constituency)
     end
   end
-
-  private
-
-  attr_reader :table
 end
