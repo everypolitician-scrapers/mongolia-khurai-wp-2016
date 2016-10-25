47ab798eded8e079f20e77b9972568960ad4a6dc
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-25 12:17:50 +0100

    Inject member class into TermPage
    TermPage is initialized with a table node. But those table nodes
    differ between member types (constituency and party list members).
    
    A member class is passed along with the table so that the correct
    member type is used for that table.

--- lib/member_table.rb
+++ lib/member_table.rb
@@ -1,17 +1,29 @@
 require_relative 'nokogiri_document'
 require_relative 'unspanned_table'
 require_relative 'khural_member'
+require_relative 'party_list_khural_member'
 
 class MemberTable < NokogiriDocument
+  def initialize(table:, member_class:)
+    super(table)
+    @member_class = member_class
+  end
+
   field :members do
     table.xpath('.//tr[td]').map do |tr|
-      KhuralMember.new(tr).to_h
+      member_class.new(tr).to_h
     end
   end
 
   private
 
+  attr_reader :member_class
+
   def table
     UnspannedTable.new(noko).transformed
+  end
+
+  def string_class
+    String
   end
 end
