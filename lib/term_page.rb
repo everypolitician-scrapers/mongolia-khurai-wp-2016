eaea5e340a9497c3944bf90fe5dd2a6f4ddaf6b1
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-25 12:15:02 +0100

    Add PartyListKhuralMember
    Class added to represent member elected from party list.
    
    The 2012 term differs from the 2008 and 2016 terms because it
    contains party list members. The party list members are listed
    in a table with a layout that differs from that of the constituency
    list members. Previously, the constituency name for each party list
    member was being set as that member's personal name.The new class returns nil for this field as party list members do not have a constituency.
    
    The class inherits directly from NokogiriDocument as fields
    created with FieldSerializer cannot be inherited currently.

--- lib/term_page.rb
+++ lib/term_page.rb
@@ -3,12 +3,16 @@ require 'nokogiri'
 
 class TermPage < NokogiriDocument
   field :members do
-    MemberTable.new(table).members
+    MemberTable.new(constituency_table).members | MemberTable.new(party_list_table).members
   end
 
   private
 
-  def table
+  def constituency_table
     noko.xpath('.//h2/span[text()[contains(.,"Constituency")]]/following::table[1]')
+  end
+
+  def party_list_table
+    noko.xpath('.//h2/span[text()[contains(.,"Party list")]]/following::table[1]')
   end
 end
