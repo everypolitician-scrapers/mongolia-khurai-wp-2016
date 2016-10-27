f547f0eddf6bcd5e5ef0671192f3b7d103b21725
Chris Mytton <chrismytton@users.noreply.github.com>
2016-10-27 13:10:12 +0100

    Extract Interaction#response_body method
    This ensures that `response.rewind` is called after reading the
    response.

--- scraper.rb
+++ scraper.rb
@@ -19,13 +19,22 @@ class Interaction
   end
 
   def noko
-    @noko ||= Nokogiri::HTML(response.read)
+    @noko ||= Nokogiri::HTML(response_body)
   end
 
   private
 
   def response
     @response ||= open(url)
+  end
+
+  def response_body
+    @response_body ||=
+      begin
+        body = response.read
+        response.rewind
+        body
+      end
   end
 
   attr_reader :url
