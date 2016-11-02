29e90f1cabc14b77439b411fab801a74553fadc7
Chris Mytton <chrismytton@gmail.com>
2016-11-02 14:42:58 +0100

    Update to latest ScrapedPage class
    I'm currently prototyping this class in parallel in the guernsey-2016
    scraper. This is the latest copy of that class based on the changes I
    made in that scraper.

--- lib/scraped_page.rb
+++ lib/scraped_page.rb
@@ -1,32 +1,56 @@
-require 'field_serializer'
+# frozen_string_literal: true
 require 'nokogiri'
 require 'open-uri'
+require 'field_serializer'
 
+# Abstract class which scrapers can extend to implement their functionality.
 class ScrapedPage
   include FieldSerializer
 
-  def initialize(url)
+  class Response
+    attr_reader :status, :headers, :body
+
+    def initialize(body:, status: 200, headers: {})
+      @status = status
+      @headers = headers
+      @body = body
+    end
+  end
+
+  module Strategy
+    class LiveRequest
+      def get(url)
+        log "Fetching #{url}"
+        response = open(url)
+        Response.new(
+          status:  response.status.first.to_i,
+          headers: response.meta,
+          body:    response.read
+        )
+      end
+
+      private
+
+      def log(message)
+        warn "[#{self.class}] #{message}"
+      end
+    end
+  end
+
+  def initialize(url:, strategy: Strategy::LiveRequest.new)
     @url = url
+    @strategy = strategy
   end
 
   private
 
+  attr_reader :url, :strategy
+
   def noko
-    @noko ||= Nokogiri::HTML(response_body)
+    @noko ||= Nokogiri::HTML(response.body)
   end
 
   def response
-    @response ||= open(url)
+    @response ||= strategy.get(url)
   end
-
-  def response_body
-    @response_body ||=
-      begin
-        body = response.read
-        response.rewind
-        body
-      end
-  end
-
-  attr_reader :url
 end
--- lib/term_page.rb
+++ lib/term_page.rb
@@ -1,4 +1,5 @@
 require_relative 'member_table'
+require_relative 'scraped_page'
 require 'nokogiri'
 
 class TermPage < ScrapedPage
