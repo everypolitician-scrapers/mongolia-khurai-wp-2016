374c5ad7cc953fe6f19f71a021ecd9c5753eb156
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 15:40:02 +0000

    Generate absolute URL once only
    Generate absolute URL before we loop over the terms instead of
    generating it twice within the loop.

--- scraper.rb
+++ scraper.rb
@@ -11,13 +11,13 @@ terms = {
   2016 => 'List_of_MPs_elected_in_the_Mongolian_legislative_election,_2016',
   2012 => 'List_of_MPs_elected_in_the_Mongolian_legislative_election,_2012',
   2008 => 'List_of_MPs_elected_in_the_Mongolian_legislative_election,_2008',
-}
+}.map { |term, url| [term, URI.join(base_url, url).to_s] }.to_h
 
 terms.each do |term, url|
-  noko = Nokogiri::HTML(open(base_url + url).read)
+  noko = Nokogiri::HTML(open(url).read)
   TermPage.new(noko).members.each do |mem|
     mem[:term] = term
-    mem[:source] = URI.join('https://en.wikipedia.org/wiki/', url).to_s
+    mem[:source] = url
     ScraperWiki.save_sqlite(%i(name term), mem)
   end
 end
