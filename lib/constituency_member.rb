1cdc63c7fe3a9b6f6c2f5e2e412eb60a8b0754ef
Tony Bowden <tony@tmtm.com>
2018-08-13 18:18:06 +0100

    Tidy text better

--- lib/constituency_member.rb
+++ lib/constituency_member.rb
@@ -2,22 +2,22 @@ require_relative 'member'
 
 class ConstituencyMember < Member
   field :name do
-    tds[-4].xpath('.//a').text.strip
+    tds[-4].xpath('.//a').text.tidy
   end
 
   field :name__mn do
-    tds[-3].text.strip
+    tds[-3].text.tidy
   end
 
   field :party do
-    tds[-1].text.strip
+    tds[-1].text.tidy
   end
 
   field :wikiname do
-    tds[-4].xpath('.//a[not(@class="new")]/@title').text.strip
+    tds[-4].xpath('.//a[not(@class="new")]/@title').text.tidy
   end
 
   field :constituency do
-    tds[0].text.strip.gsub("\n", ' — ')
+    tds[0].xpath('*').map(&:text).map(&:tidy).reject(&:empty?).join(' - ')
   end
 end
